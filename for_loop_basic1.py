ce2f4cb46de0990ad8f7dc9ed68eed9a83fbe6b3
nobleat <88677746+nobleat@users.noreply.github.com>
2021-11-04 12:29:20 -0400

    Add files via upload

---
+++ for_loop_basic1.py
@@ -0,0 +1,29 @@
+for x in range(151):
+    print(x)
+
+for f in range(0,1000,5):
+    print(f)
+
+
+for d in range(1,101):
+    if d%10==0:
+        print("Coding Dojo")
+    elif d%5==0:
+        print("Coding")
+    else:
+        print(d)
+
+sum=0
+for w in range(0,500000):
+    sum+=w
+print(sum)
+
+for c in range(2018,0,-4):
+    print(c)
+
+lowNum=3
+highNum=369
+mult=3
+for a in range(3,369):
+    if a%3==0:
+        print(a)
